56ff1e31437db2a09da97526586b720f32e2e392
Xiaochen Xu <xiaochex@gmail.com>
2021-05-19 14:57:07 -0400

    commit tree 85bd26b96b5f6c746399b3f96e0d4a41bba11e80

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    # self.split.select{|w|w.sentence? || w.question? || w.exclamation?}.size
+    self.split(/\.|\?|\!/).select{|w|w != ""}.size
   end
 end
